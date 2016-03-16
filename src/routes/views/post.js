7a13ade653ea527a33283d95e2b799d6bb56a778
Ming Li <mli@rocketsoftware.com>
2016-03-16 17:05:10 +0800

    fixed a route bug

--- src/routes/views/post.js
+++ src/routes/views/post.js
@@ -1,1 +1,16 @@
 // TODO:
+
+'use strict';
+
+const keystone = require('keystone');
+const Post = keystone.list('Post');
+
+exports = module.exports = function(req, res) {
+  const view = new keystone.View(req, res);
+
+  view.on('init', function(next) {
+
+  });
+
+  view.render('post');
+};
