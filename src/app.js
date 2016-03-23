b670a20111f593e909301898d1c2108b0b729539
Ming Li <mli@rocketsoftware.com>
2016-03-23 22:38:32 +0800

    1. fixed editor problem. 2. polish the style.

--- src/app.js
+++ src/app.js
@@ -29,23 +29,8 @@ keystone.init({
 
   'auto update': true,
 
-
   // editor configuration
-  'wysiwyg override toolbar': false,
-  'wysiwyg menubar': false,
-  'wysiwyg additional buttons': 'styleselect, blockquote, searchreplace ,'
-    + ' forecolor backcolor,  media, preview print ',
-  'wysiwyg additional plugins': 'example, anchor,'
-    + ' autolink, autosave, bbcode, charmap, contextmenu, '
-    + ' directionality, emoticons, fullpage, hr, media, '
-    + ' paste, preview, print, searchreplace, textcolor'
-  /*'wysiwyg additional options': {
-    external_plugins: {
-      relative_urls: false,
-      visualblocks_default_state: true,
-      'stylebuttons':'public/js/tinymce/plugins/stylebuttons/plugin.js'
-    }
-  }*/
+  'wysiwyg additional buttons': 'styleselect'
 });
 
 keystone.import('models');
